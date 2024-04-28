b867898820a380954076020a361238118c3d6545
Pavan Putta <pavanputta999@gmail.com>
2024-04-28 17:02:17 +0530

    initial commit

---
+++ react-portfolio/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      text: "#fff",
+      primary: "#576cbc",
+      secondary: "#19376d",
+      dark: "#0b2447",
+      bgColor: "#04152d",
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
